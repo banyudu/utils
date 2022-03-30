ae8b984d83f8cf51188a4e522815ccc9a8250e7c
banyudu <banyudu@gmail.com>
2022-03-30 21:12:40 +0800

    feat: 修改首页和全局样式

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -11,11 +11,11 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children, title, showGithub = true }) => {
   const router = useRouter()
   return (
-    <div className='w-screen h-screen max-w-full'>
+    <div className='w-screen h-screen max-w-full text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900'>
       <Head>
         <title>{title}</title>
       </Head>
-      <div className='prose w-full max-w-6xl px-12 py-8 mx-auto'>
+      <div className='prose dark:prose-invert w-full max-w-6xl px-12 py-8 mx-auto'>
         <div className='flex flex-row items-center mb-8 justify-between'>
           <h1 className='m-0'>{title}</h1>
           <a
